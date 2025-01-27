52d2ca2144a909123f78db1b010c4307510ee605
Harrison Newman <harrydnewman@gmail.com>
2025-01-27 16:38:38 -0500

    list of stuff to work on

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -6,9 +6,15 @@ export default function Gallery(){
             <h1>Gallery</h1>
 
             {/* items:
-                - zipcode clock
-                - chattech
-                - brainrot archive
+                - zipcode clock (code)
+                - chattech (code + museums??)
+                - brainrot archive (code)
+                - catalist (ux)
+                - hell on earth (code + game design)
+                - velociraptor (physical/cnc)
+                - truth or dare (storytelling)
+                - cunty roads (p comp)
+                - german population thing (graphing)
                 - other shit
                  */}
         </div>
