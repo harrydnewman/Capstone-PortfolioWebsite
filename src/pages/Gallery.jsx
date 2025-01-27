1287287e26e4a6684fe5c7415da9a58c99da6924
Harrison Newman <harrydnewman@gmail.com>
2025-01-27 14:40:54 -0500

    updated zipcode clock

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -4,6 +4,13 @@ export default function Gallery(){
     return (
         <div>
             <h1>Gallery</h1>
+
+            {/* items:
+                - zipcode clock
+                - chattech
+                - brainrot archive
+                - other shit
+                 */}
         </div>
     )
 }
