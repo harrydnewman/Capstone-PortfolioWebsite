210ae8a3877ed9f930fe1694d2fc8bdbb72d6340
Harrison Newman <harrydnewman@gmail.com>
2025-01-28 16:43:03 -0500

    updating routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,55 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Gallery from "./pages/Gallery";
-import Zipcodeclock from "./pages/portfoliopages/ZipCodeClock";
+
 import Blog from "./pages/Blog";
 
-export default function App(){
+// portfolio pages
+
+import BrainrotArchive from "./pages/portfoliopages/BrainrotArchive";
+import Catalist from "./pages/portfoliopages/Catalist";
+import ChatTech from "./pages/portfoliopages/ChatTech";
+import Crossword from "./pages/portfoliopages/Crossword";
+import CuntyRoads from "./pages/portfoliopages/CuntyRoads";
+import GermanPopulation from "./pages/portfoliopages/GermanPop";
+import HellOnEarth from "./pages/portfoliopages/HellOnEarth";
+import IEFinal from "./pages/portfoliopages/IEFinal";
+import IEMidterm from "./pages/portfoliopages/IEMidterm";
+import InventoryManagement from "./pages/portfoliopages/InventoryManagement";
+import JimmyBuffet from "./pages/portfoliopages/JimmyBuffet";
+import MastodonBot from "./pages/portfoliopages/MastodonBot";
+import SubwaySafety from "./pages/portfoliopages/SubwaySafety";
+import Taskr from "./pages/portfoliopages/Taskr";
+import TruthOrDare from "./pages/portfoliopages/TruthOrDare";
+import Velociraptor from "./pages/portfoliopages/Velociraptor";
+import Zipcodeclock from "./pages/portfoliopages/ZipCodeClock";
+
+export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/portfolio" element={<Gallery/>}/>
-        <Route path="/portfolio/zipcodeclock" element={<Zipcodeclock/>}/>
-        <Route path="/blog" element={<Blog/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/portfolio" element={<Gallery />} />
+
+        <Route path="/blog" element={<Blog />} />
+        {/* portfolio routes */}
+        <Route path="/portfolio/brainrotarchive" element={<BrainrotArchive />} />
+        <Route path="/portfolio/catalist" element={<Catalist/>}/>
+        <Route path="/portfolio/chattech" element={<ChatTech />} />
+        <Route path="/portfolio/crossword" element={<Crossword />} />
+        <Route path="/portfolio/cuntyroads" element={<CuntyRoads />} />
+        <Route path="/portfolio/germanpopulation" element={<GermanPopulation />} />
+        <Route path="/portfolio/hellonearth" element={<HellOnEarth />} />
+        <Route path="/portfolio/iefinal" element={<IEFinal />} />
+        <Route path="/portfolio/iemidterm" element={<IEMidterm />} />
+        <Route path="/portfolio/inventorymanagement" element={<InventoryManagement />} />
+        <Route path="/portfolio/jimmybuffet" element={<JimmyBuffet />} />
+        <Route path="/portfolio/mastodonbot" element={<MastodonBot />} />
+        <Route path="/portfolio/subwaysafety" element={<SubwaySafety />} />
+        <Route path="/portfolio/taskr" element={<Taskr />} />
+        <Route path="/portfolio/truthordare" element={<TruthOrDare />} />
+        <Route path="/portfolio/velociraptor" element={<Velociraptor />} />
+        <Route path="/portfolio/zipcodeclock" element={<Zipcodeclock />} />
       </Routes>
     </Router>
   )
---
+++ src/pages/portfoliopages/Catalist.jsx
@@ -0,0 +1,7 @@
+export default function Catalist(){
+    return (
+        <div>
+            <h1>Catalist</h1>
+        </div>
+    )
+}
