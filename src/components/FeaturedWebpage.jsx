8b5c480e44caad16eed5f4a2ed6450f13d9dee42
Harrison Newman <harrydnewman@gmail.com>
2025-01-29 22:41:07 -0500

    finished chat tech

--- src/components/FeaturedWebpage.jsx
+++ src/components/FeaturedWebpage.jsx
@@ -1,13 +1,13 @@
 import styles from '../styles/FeaturedWebpage.module.css'
 
-export default function FeaturedWebpage({ image, title, points = [] }) {
+export default function FeaturedWebpage({ image, title, points = [], paragraphs = [] }) {
     return (
         <div className={styles.main}>
             <div className={styles.image}>
                 <img src={image} alt={title} />
             </div>
             <div className={styles.description}>
-            <h2>{title}</h2>
+            <h2 style={{ marginLeft: points.length > 0 ? '25px' : '0px' }}>{title}</h2>
                 {points.length > 0 && (
                     <>
                         <ul>
@@ -15,6 +15,13 @@ export default function FeaturedWebpage({ image, title, points = [] }) {
                                 <li key={index}>{point}</li>
                             ))}
                         </ul>
+                    </>
+                )}
+                { paragraphs.length > 0 && (
+                    <>
+                        {paragraphs.map((paragraph, index) => (
+                            <p key={index}>{paragraph}</p>
+                        ))}
                     </>
                 )}
             </div>
