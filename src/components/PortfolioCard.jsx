563ba42332b404ee876375eb0c5bfa65f436513f
Harrison Newman <harrydnewman@gmail.com>
2025-01-30 15:51:24 -0500

    basic ideas for portfolio gallery are handled, move onto next thing

--- src/components/PortfolioCard.jsx
+++ src/components/PortfolioCard.jsx
@@ -1,20 +1,45 @@
 /* eslint-disable react/prop-types */
-import styles from '../styles/PortfolioCard.module.css'
-export default function PortfolioCard({ portfolioItem }) {
-    return (
-        <div className={styles.PortfolioCardDiv}>
-            {portfolioItem.iframesrc &&
-                <div className={styles.iframeContainer}>
-                <iframe src={portfolioItem.iframesrc}></iframe>
-                </div>
-            }
-            {portfolioItem.image &&
-                <div className={styles.imageContainer}>
-                            <img src={portfolioItem.image} alt={portfolioItem.altText} />
-                        </div>
-            }
-            <h2>{portfolioItem.title}</h2>
-        </div>
-    )
+import styles from '../styles/PortfolioCard.module.css';
+import { useState } from 'react';
 
+export default function PortfolioCard({ portfolioItem }) {
+    const [hovered, setHovered] = useState(false);
+
+    return (
+        <div
+            className={`${styles.PortfolioCardDiv} ${hovered ? styles.expanded : ''}`}
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+        >
+            {portfolioItem.iframesrc && portfolioItem.link && (
+                <a href={portfolioItem.link} >
+                <div className={styles.iframeContainer}>
+                    <iframe src={portfolioItem.iframesrc}></iframe>
+                </div>
+                </a>
+            )}
+            {portfolioItem.image && portfolioItem.link && (
+                <a href={portfolioItem.link} >
+                <div className={styles.imageContainer}>
+                    <img src={portfolioItem.image} alt={portfolioItem.altText} />
+                </div>
+                </a>
+            )}
+            <h2>{portfolioItem.title}</h2>
+            <div className={styles.linkContainer}>
+                {portfolioItem.date && (
+                    <h3 className={styles.link}>{portfolioItem.date}</h3>
+                )}
+                {portfolioItem.liveLink && (
+                    <a href={portfolioItem.liveLink} target='_blank'>
+                    <div className={styles.linkButton}>
+                        <p>View Live</p>
+                    </div>
+                    </a>
+                )}
+                
+
+            </div>
+        </div>
+    );
 }
