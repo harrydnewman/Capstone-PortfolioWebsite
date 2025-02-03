f99410331b2fad546d28ef08edfc668734e9a802
Harrison Newman <harrydnewman@gmail.com>
2025-02-02 21:19:54 -0500

    homepage done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,9 @@ export default function Footer() {
         <div className={styles.FooterDiv}>
             <div className={styles.siteTitle}>
                 <a href='/'>Harrison Newman</a>
+                
             </div>
+            <a href='/login'>Login</a>
             <div className={styles.links}>
                 <a href='/portfolio'>Portfolio</a>
                 <a href='/blog'>Blog</a>
