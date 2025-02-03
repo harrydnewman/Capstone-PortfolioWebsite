eb2a084d2ec0a6020154ec62249db53372c4f4b7
Harrison Newman <harrydnewman@gmail.com>
2025-02-03 09:19:13 -0500

    updated home page

--- src/components/SkillIcon.jsx
+++ src/components/SkillIcon.jsx
@@ -1,10 +1,20 @@
-import styles from '../styles/SkillIcon.module.css'
+import { useRef, useEffect, useState } from 'react';
+import styles from '../styles/Home.module.css';
 
-export default function SkillIcon({image, skill}){
-    // come back and add like if hovering over then it does shit
+export default function SkillIcon({ image, skill }) {
+    const textRef = useRef(null);
+    const [textWidth, setTextWidth] = useState(0);
+
+    useEffect(() => {
+        if (textRef.current) {
+            setTextWidth(textRef.current.offsetWidth);
+        }
+    }, []);
+
     return (
-        <div className={styles.skillIcon}>
-            <img src={image} />
+        <div className={styles.skillIcon} style={{ "--text-width": `${textWidth}px` }}>
+            <img src={image} alt={skill} />
+            <div ref={textRef} className={styles.skillText}>{skill}</div>
         </div>
-    )
+    );
 }
