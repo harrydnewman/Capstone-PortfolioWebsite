9f32cff9e77ea4a25061268640eaa980d989f5e5
Harrison Newman <harrydnewman@gmail.com>
2025-02-02 20:49:36 -0500

    backup

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import styles from '../styles/Header.module.css'
 export default function Header() {
     return (
         <div className={styles.HeaderDiv}>
-        <div className={styles.siteTitle}>
-        <a href='/'>Harrison Newman</a>
-        </div>
+            
+                <a href='/'>Harrison Newman</a>
+           
             <div className={styles.links}>
                 <a href='/portfolio'>Portfolio</a>
                 <a href='/blog'>Blog</a>
